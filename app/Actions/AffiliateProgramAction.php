43a26ea69b24768001c5b5c1c93610e1132e2f74
MaxTitovitch <maxtitovitch@mail.ru>
2020-09-04 02:57:59 +0300

    Mobile typeset added

---
+++ app/Actions/AffiliateProgramAction.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Actions;
+
+use TCG\Voyager\Actions\AbstractAction;
+
+class AffiliateProgramAction extends AbstractAction
+{
+    public function getTitle()
+    {
+        return 'Affiliate';
+    }
+
+    public function getIcon()
+    {
+        return 'voyager-eye';
+    }
+
+    public function getPolicy()
+    {
+        return 'read';
+    }
+
+    public function getAttributes()
+    {
+        return [
+            'style' => 'margin-left: 2px; margin-top 2px; clear: both;',
+            'class' => 'btn btn-sm btn-dark pull-right view',
+        ];
+    }
+
+    public function getDefaultRoute()
+    {
+        return route('voyager.contacts.index', ['key' => 'brand_id', 'filter' => 'equals', 's' => $this->data->id]);
+    }
+
+    public function shouldActionDisplayOnDataType()
+    {
+        return $this->dataType->slug == 'brands';
+    }
+}
---
+++ app/ProgramValue.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProgramValue extends Model
+{
+    //
+}
